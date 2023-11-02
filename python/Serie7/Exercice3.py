43340aaa8c24ac92bf17c4583cdbce082beeb68d
Ibrahim Ouhamou <ouhamouy10@gmail.com>
2023-11-02 21:30:30 +0100

    BismiAllah

--- python/Serie7/Exercice3.py
+++ python/Serie7/Exercice3.py
@@ -2,7 +2,12 @@
 print("in the name of Allah")
 
 def chif_number(num):
-    return len(str(num))
+    i = 1
+    while num > 10:
+        i += 1
+        num = num // 10
+    return i
+    #return len(str(num))
 
 num = int(input("entrer un nombre: "))
 print(num, "a", chif_number(num), "chiffres")
