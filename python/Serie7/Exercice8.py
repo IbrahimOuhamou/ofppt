a68f55e76e44e41bf247aedd33d890d08b62334b
Ibrahim Ouhamou <ouhamouy10@gmail.com>
2023-11-10 09:26:13 +0100

    BimiAllah

--- python/Serie7/Exercice8.py
+++ python/Serie7/Exercice8.py
@@ -5,8 +5,7 @@ print("in the name of Allah")
 def U(num):
     if (num == 1) or (num == 2):
         return 1
-    print(U(num - 1), U(num - 2), ":", num -1, num -2)
-    return U(num - 1) + U(num -2)
+    return (U(num - 1) + U(num -2))
 
 num = int(input("enter a number: "))
 
