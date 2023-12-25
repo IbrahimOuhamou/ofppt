8976abd521ef562fccbb7e4bc16a77cbbe152b07
Ibrahim Ouhamou <ouhamouy10@gmail.com>
2023-12-25 11:56:04 +0100

    alhamdo li Allah

---
+++ algo-python/python/dictionnaire.py
@@ -0,0 +1,17 @@
+#بسم الله الرحمن الرحيم
+
+print("بسم الله الرحمن الرحيم")
+
+dict = {'key':'value', 'key1':'value1', 'key2':'valu2', 'key3':'value3'}
+print(dict)
+
+for elm in dict:
+    print(elm)
+    print(dict[elm])
+
+for elm in dict.keys():
+    print(elm)
+
+
+for value in dict.items():
+    print(value)
