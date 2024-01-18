0304080598e67046bdaef3727e2680a1b131a82e
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-01-18 12:12:46 +0100

    alhamdo li Allah

--- poo/Exercice2/livre.py
+++ poo/Exercice2/livre.py
@@ -1,9 +1,28 @@
 #in the name of Allah
 
 class livre:
+    counter = 0
     def __init__(self, titre, auteur, prix, annee):
         self.titre = titre
         self.auteur = auteur 
         self.prix = prix
         self.annee = annee
+        livre.counter += 1
+
+    def compter(self):
+        return livre.counter
+
+    def afficher(self):
+        print('titre:', self.titre)
+        print('auteur:', self.auteur)
+        print('prix:', self.prix)
+        print('annee:', self.annee)
+
+    def type(self):
+        if self.prix < 100:
+            return 1
+        elif self.prix <= 500:
+            return 2
+        else:
+            return 3
 
--- poo/Exercice2/main.py
+++ poo/Exercice2/main.py
@@ -1,6 +1,26 @@
 #in the name of Allah
 
-if __name__ = '__main__':
+from livre import livre
+
+if __name__ == '__main__':
     print("in the name of Allah")
+    livre1 = livre('l1', 'a1', 200, 2012)
+    livre1.afficher()
+    print('type', livre1.type())
+
+    livre2 = livre('l2', 'a2', 200, 2022)
+    livre2.afficher()
+    print('type', livre2.type())
+
+    livre3 = livre('l3', 'a3', 200, 2032)
+    livre3.afficher()
+    print('type', livre3.type())
+
+    livre4 = livre('l4', 'a4', 200, 2042)
+    livre4.afficher()
+    print('type', livre4.type())
+
+    print('le nombre des instances est', livre1.compter())
+    print('le nombre des instances est', livre.counter)
 
 
