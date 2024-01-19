6b0fdc6215dc3b12feb761974e29c9b8f0024608
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-01-19 15:18:49 +0100

    alhamdo li Allah

---
+++ poo/python/heritage/classes.py
@@ -0,0 +1,27 @@
+#in the name of Allah
+
+class person():
+    def __init__(self, nom1, cin1, date_naissance1):
+        self.nom = nom1
+        self.cin = cin1
+        self.date_naissance = date_naissance1
+
+    def afficher(self):
+        print(self.nom, self.cin, self.date_naissance)
+
+    def age(self):
+        a = 2024 - self.date_naissance
+        return a
+
+class etudiant(person):
+    def __init__(self, nom1, cin1, date_naissance1, note1, note2):
+        #super().__init__(nom1, cin1, date_naissance1):
+        person.__init__(self, nom1, cin1, date_naissance1)
+        self.note1 = note1
+        self.note2 = note2
+
+    def afficher(self):
+        super().afficher()
+        #person.afficher(self)
+        print(self.note1, self.note2)
+
---
+++ poo/python/heritage/main.py
@@ -0,0 +1,8 @@
+#in the name of Allah
+
+from classes import person, etudiant
+
+if __name__ == "__main__":
+    e1 = etudiant('rbati', 'A3712', 2000, 13, 17)
+    e1.afficher()
+
