36c85896e75ed8bde679637fa96e393c13f705a7
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-01-29 09:21:59 +0100

    alhamdo li Allah

--- poo/python/heritage/classes.py
+++ poo/python/heritage/classes.py
@@ -1,5 +1,12 @@
 #in the name of Allah
 
+class etre_vivant():
+    def __init__(self, n_cellules):
+        self.n_cellules = n_cellules
+    def respirer(self):
+        print("respirer")
+
+
 class person():
     def __init__(self, nom1, cin1, date_naissance1):
         self.nom = nom1
@@ -13,10 +20,11 @@ class person():
         a = 2024 - self.date_naissance
         return a
 
-class etudiant(person):
-    def __init__(self, nom1, cin1, date_naissance1, note1, note2):
+class etudiant(etre_vivant, person):
+    def __init__(self, n_cellules, nom1, cin1, date_naissance1, note1, note2):
         #super().__init__(nom1, cin1, date_naissance1):
         person.__init__(self, nom1, cin1, date_naissance1)
+        etre_vivant.__init__(self, n_cellules)
         self.note1 = note1
         self.note2 = note2
 
--- poo/python/heritage/main.py
+++ poo/python/heritage/main.py
@@ -3,6 +3,8 @@
 from classes import person, etudiant
 
 if __name__ == "__main__":
-    e1 = etudiant('rbati', 'A3712', 2000, 13, 17)
+    e1 = etudiant(10000, 'rbati', 'A3712', 2000, 13, 17)
     e1.afficher()
+    e1.respirer()
+    print(e1.nom, "a", e1.n_cellules, "cellules")
 
