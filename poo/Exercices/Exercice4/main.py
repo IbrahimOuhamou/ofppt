0cccc85deb75ca23c92c971db7bcf0f0faa9d1b0
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-01-30 15:18:30 +0100

    alhamdo li Allah

--- poo/Exercices/Exercice4/main.py
+++ poo/Exercices/Exercice4/main.py
@@ -1,6 +1,8 @@
 #in the name of Allah
 
-from rectangle import rectangle
+from pokimon import pokimon_sportif, pokimon_casanier, pokimon_mer
 
 if __name__ == '__main__':
     print("in the name of Allah")
+    s = pokimon_sportif("pokimon", 19, 4, 100)
+    print(s)
--- poo/Exercices/Exercice4/pokimon.py
+++ poo/Exercices/Exercice4/pokimon.py
@@ -4,13 +4,20 @@ class pokimon():
     def __init__(self, nom, poids):
         self.nom = nom
         self.poids = poids
+    def vitesse(self):
+        pass
+    def __str__(self):
+        return "je suis le pokimon " + str(self.nom) + " mon poids est " + str(self.poids)
 
 class pokimon_sportif(pokimon):
     def __init__(self, nom, poids, n_pattes, freq_cardique):
         pokimon.__init__(self, nom, poids)
         self.n_pattes = n_pattes
-        self.freq_cardique = freq_cardiqu
-    #self.vitesse = pself.oids * tself.aille * 3
+        self.freq_cardique = freq_cardique
+    def vitesse(self):
+        return self.poids * self.taille * 3
+    def __str__(self):
+        return pokimon.__str__(self) + " mon nombre des pattes est " + str(self.n_pattes) + " ma frequance cardique est " + str(self.freq_cardique)
 
 class pokimon_casanier(pokimon):
     def __init__(self, nom, poids, n_pattes, taille, regarde_temps):
@@ -18,11 +25,17 @@ class pokimon_casanier(pokimon):
         self.n_pattes = n_pattes
         self.taille = taille
         self.regarde_temps = regard_temps
-    #self.vitesse = self.n_pattes * self.taille * 3
+    def vitesse(self):
+        return self.n_pattes * self.taille * 3
+    def __str__(self):
+        return pokimon.__str__(self) + " mon nombre des pattes est " + str(self.n_pattes) + " ma taille est " + str(self.taille) + " mon temps de regard " + str(self.regarde_temps)
 
 class pokimon_mer(pokimon):
     def __init__(self, nom, poids, nageoire):
         pokimon.__init__(self, nom, poids)
         self.nageoire = nageoire
-    #self.vitesse = self.poids / 25 * self.nageoire
+    def vitesse(self):
+        return self.poids / 25 * self.nageoire
+    def __str__(self):
+        return pokimon.__str__(self) + " mon nombre des pattes est " + str(self.n_pattes) + " nageoire " + str(self.nageoire)
 
