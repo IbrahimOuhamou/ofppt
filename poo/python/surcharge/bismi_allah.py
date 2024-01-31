4927beb0d13541ac9174e3e43fb99265e020bbd6
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-01-31 09:50:10 +0100

    la ilaha illa Allah

--- poo/python/surcharge/bismi_allah.py
+++ poo/python/surcharge/bismi_allah.py
@@ -1,22 +1,31 @@
 #in the name of Allah
 
+x = 78
+
 class point_t():
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
 
     def __add__(self, p):
         x = self.x + p.x
         y = self.y + p.y
         return point_t(x, y)
 
-    def __str__(self):
-        return "(" + str(self.x) + ", " + str(self.y) + ")"
+    def __eq__(self, p):
+        if self.x == p.x and self.y == p.y:
+            return True
+        else:
+            return False
 
 
 print("in the name of Allah")
 
 vec2_1 = point_t(4, 18)
-vec2_2 = point_t(-4, 18)
+vec2_2 = point_t()
 print(str(vec2_1 + vec2_2))
+print(vec2_1 == vec2_2)
 
