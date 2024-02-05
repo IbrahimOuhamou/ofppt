23db39a8ff266565eeaa74db9c5768f6d885061b
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-02-05 13:36:15 +0100

    la ilaha illa Allah

---
+++ poo/Exercices/Exercice8/bismi_allah.py
@@ -0,0 +1,17 @@
+#in the name of Allah
+
+import tkinter as tk
+
+print("in the name of Allah")
+
+app = tk.Tk()
+
+label1 = tk.Label(app, text="Number1: ")
+label2 = tk.Label(app, text="Number2: ")
+label_result = tk.Label(app, text="Result: ")
+entry1 = tk.Entry(app)
+entry2 = tk.Entry(app)
+entry_operation = tk.Entry(app)
+
+app.mainloop()
+
