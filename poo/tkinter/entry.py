7cde9846cad20165d1b52daf407cf1af008a9fdf
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-02-05 10:54:26 +0100

    la ilaha illa Allah

--- poo/tkinter/entry.py
+++ poo/tkinter/entry.py
@@ -10,6 +10,7 @@ app = tk.Tk()
 entry = tk.Entry()
 button_insert = tk.Button(text="inna li Allah wa inna ilayhi raji3on, ")
 button_delete = tk.Button(text="la ilaha illa Allah")
+button_afficher = tk.Button(text="afficher")
 
 def m_insert():
     entry.insert(0, "inna li Allah wa inna ilayhi raji3on, ")
@@ -18,13 +19,18 @@ def m_delete():
     c = entry.get()
     entry.delete(0, len(c))
     
+def m_afficher():
+    label = tk.Label(app, text=entry.get())
+    label.pack()
 
 entry.pack()
 button_insert.pack()
 button_delete.pack()
+button_afficher.pack()
 
 button_insert.config(command=m_insert)
 button_delete.config(command=m_delete)
+button_afficher.config(command=m_afficher)
 
 app.mainloop()
 
