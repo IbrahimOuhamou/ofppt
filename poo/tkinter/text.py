f858fb815c9809e507ada05f64c977be57202ace
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-02-12 09:26:40 +0100

    la ilaha illa Allah

--- poo/tkinter/text.py
+++ poo/tkinter/text.py
@@ -13,6 +13,8 @@ text.place(x=100, y=10)
 
 def text_clear():
     text.delete(0.0, tk.END)
+    #lire = text.get("0.7", "4.8")
+    text.insert(tk.INSERT, "inna li Allah wa inna ilayhi raji3on")
 
 tk.Button(app, text="la ilaha illa Allah", command=text_clear).place(x=100, y=180)
 
