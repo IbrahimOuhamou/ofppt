1ebee7c7a69ba0d0e5e98d1822ee343ae68fe9b9
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-02-12 09:22:51 +0100

    la ilaha illa Allah

--- poo/tkinter/text.py
+++ poo/tkinter/text.py
@@ -11,6 +11,10 @@ label.place(x=0, y=10)
 text = tk.Text(app, height=10, width=30)
 text.place(x=100, y=10)
 
+def text_clear():
+    text.delete(0.0, tk.END)
+
+tk.Butoon(app, text="la ilaha illa ALlah", command=text_clear).place(x=100, y=40)
 
 app.mainloop()
 
