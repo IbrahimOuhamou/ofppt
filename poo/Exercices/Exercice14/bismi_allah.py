48935d479aab015100d9904185d9a252b27b6672
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-02-23 12:20:43 +0100

    la ilaha illa Allah

--- poo/Exercices/Exercice14/bismi_allah.py
+++ poo/Exercices/Exercice14/bismi_allah.py
@@ -1,6 +1,7 @@
 #in the name of Allah
 
 import tkinter as tk
+import re
 
 print("in the name of Allah")
 
@@ -12,22 +13,27 @@ login_label_name.place(x=20, y=20)
 login_entry_name = tk.Entry(login_window)
 login_entry_name.place(x=80, y=20)
 
-login_label_password = tk.Label(login_window, text="mot de pass:")
-login_label_password.place(x=0, y=40)
-login_entry_password = tk.Entry(login_window)
-login_entry_password.place(x=80, y=40)
+login_label_email = tk.Label(login_window, text="email: ")
+login_label_email.place(x=0, y=40)
+login_entry_email = tk.Entry(login_window)
+login_entry_email.place(x=80, y=40)
+
+login_label_telephoone = tk.Label(login_window, text="telephoone: ")
+login_label_telephoone.place(x=0, y=40)
+login_entry_telephoone = tk.Entry(login_window)
+login_entry_telephoone.place(x=80, y=40)
 
 login_button_clear = tk.Button(login_window, text="effacer")
-login_button_clear.place(x=80, y=60)
+login_button_clear.place(x=80, y=100)
 login_button_connect = tk.Button(login_window, text="Se connecter")
-login_button_connect.place(x=150, y=60)
+login_button_connect.place(x=150, y=100)
 
 def clear():
     login_entry_name.delete(0, tk.END)
-    login_entry_password.delete(0, tk.END)
+    login_entry_email.delete(0, tk.END)
 
 def connect():
-    if login_entry_name.get() != "User" or login_entry_password.get() != "123":
+    if login_entry_email.get() != "123" and re.search():
         tk.Label(login_window, text="mot de pass incorrect", fg="red").place(x=80, y=90)
         return
     print("alhamdo li Allah")
