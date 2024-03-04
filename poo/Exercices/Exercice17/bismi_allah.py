f495638c1d50ca76d6baf5b1d84ed558246b3c09
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-04 10:29:38 +0100

    la ilaha illa Allah

--- poo/Exercices/Exercice17/bismi_allah.py
+++ poo/Exercices/Exercice17/bismi_allah.py
@@ -3,6 +3,7 @@
 print("in the name of Allah")
 
 import tkinter as tk
+from tkinter import messagebox
 
 window = tk.Tk()
 window.geometry("300x300")
@@ -16,23 +17,28 @@ for i in range(len(menu_text)):
     menu_checkbuttons.insert(i, tk.Checkbutton(window, text=menu_text[i], variable=menu_intvars[i]))
     menu_checkbuttons[i].place(x = 20 + 90*(i%2), y = 30 + 20*i - (20 * (i % 2)))
 
-text = tk.Text(window, height=10, width=10)
+text = tk.Text(window, height=10, width=10, state="disabled")
 text.place(x=200, y=20)
 
 
 def raz():
-    text.delete(0.0, tk.END)
-    #for var in menu_intvars:
-        #var.set(0)
-    for var in menu_checkbuttons:
-        var.deselect()
+    if tk.messagebox.askokcancel("la ilaha illa Allah", "bismi Allah\nVoulez vous vraiment remise a zero"):
+        text.config(state="normal")
+        text.delete(0.0, tk.END)
+        text.config(state="disabled")
+        #for var in menu_intvars:
+            #var.set(0)
+        for checkbox in menu_checkbuttons:
+            checkbox.deselect()
         
 
 def ok():
+    text.config(state="normal")
     text.delete(0.0, tk.END)
     for i in range(len(menu_text)):
         if(menu_intvars[i].get() == 1):
             text.insert(tk.INSERT, menu_text[i] + "\n")
+    text.config(state="disabled")
 
 def fermer():
     window.destroy()
