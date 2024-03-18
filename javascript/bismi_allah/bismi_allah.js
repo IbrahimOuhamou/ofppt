0bcda4934b58fa1beb929a04fa851ea8e66290bd
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-18 15:18:18 +0000

    la ilaha illa Allah mohammed rassoul Allah

--- javascript/bismi_allah/bismi_allah.js
+++ javascript/bismi_allah/bismi_allah.js
@@ -6,4 +6,8 @@ document.getElementById("bismi_allah").innerHTML = "la ilaha illa Allah mohammed
 alert(5 + 6);
 console.log("la ilaha illa Allah mohammed rassoul Allah");
 
+window.alert("la ilaha illa Allah mohammed rassoul Allah");
+let bismi_allah_var = window.prompt("what text you want?");
+window.alert(bismi_allah_var);
+
 
