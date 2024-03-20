104d12d01c9ed8bc7c684f4512ea28094a216c5b
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-20 15:38:06 +0000

    la ilaha illa Allah Mohammed messenger of Allah

---
+++ php/bismi_allah.php
@@ -0,0 +1,16 @@
+<?php
+//in the name of Allah
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+    <title>in the name of Allah</title>
+</head>
+<body>
+    <p>in the name of Allah</p>
+</body>
+</html>
+';
+
+?>
