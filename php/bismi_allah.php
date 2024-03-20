3e2a7416c17f63648a04ecb2a72e3ea2e8c77059
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-20 18:06:34 +0000

    la ilaha illa ALlah

--- php/bismi_allah.php
+++ php/bismi_allah.php
@@ -1,16 +1,11 @@
-<?php
-//in the name of Allah
-
-echo '
 <!DOCTYPE html>
+<!--in the name of Allah-->
 <html>
 <head>
     <title>in the name of Allah</title>
 </head>
 <body>
     <p>in the name of Allah</p>
+    <p><?php time() ?></p>
 </body>
 </html>
-';
-
-?>
