e6eb6492dfa7b94969984239e898c2bf6f0433d2
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-21 12:20:32 +0000

    alhamdo li Allah

--- javascript/bismi_allah/bismi_allah.js
+++ javascript/bismi_allah/bismi_allah.js
@@ -19,3 +19,43 @@ alert(variable1 + " * " + variable2 + " == " + (variable1 * variable2));
 alert(variable1 + " / " + variable2 + " == " + (variable1 / variable2));
 alert(variable1 + " % " + variable2 + " == " + (variable1 % variable2));
 
+if(variable1 > variable2)
+{
+    alert(variable1 + " > " + variable2)
+}
+else
+{
+    alert(variable1 + " < " + variable2)
+}
+
+if(variable1 > 5)
+{
+    alert(variable1 + " > 5")
+}
+else
+{
+    alert(variable1 + " < 5")
+}
+
+let moyenne = parseInt(prompt("donner le moyenne"));
+if(moyenne < 0 || moyenne > 20)
+{
+    alert('moyenne invalide');
+}
+else if(16 <= moyenne)
+{
+    alert('Tres bien');
+}
+else if(14 <= moyenne)
+{
+    alert('Bien');
+}
+else if(12 <= moyenne)
+{
+    alert('Assez bien');
+}
+else if(10 <= moyenne)
+{
+    alert('Passable');
+}
+
