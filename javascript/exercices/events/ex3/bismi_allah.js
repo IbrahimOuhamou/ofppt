402f409e7c3b036a003b8c8a991280abf1e12d19
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-05 10:17:26 +0000

    la ilaha illa Allah

--- javascript/exercices/events/ex3/bismi_allah.js
+++ javascript/exercices/events/ex3/bismi_allah.js
@@ -1,12 +1,18 @@
 //in the name of Allah
 //la ilaha illa Allah mohammed rassoul Allah
 
-document.getElementById('bismi_allah_img').onmouseover = function()
-    {
-        document.getElementById('bismi_allah_img').src = 'dikr_image.jpg';
-    }
+document.getElementById('bismi_allah').onfocus = function()
+{
+    this.style.color = 'red'
+}
 
-document.getElementById('bismi_allah_img').onmouseout = function()
-    {
-        document.getElementById('bismi_allah_img').src = 'Sway_Wallpaper_Blue_1920x1080.png';
-    }
+document.getElementById('bismi_allah').onblur = function()
+{
+    this.style.color = 'black'
+}
+
+document.getElementById('bismi_allah').onchange = function()
+{
+    this.style.color = 'green'
+}
+
