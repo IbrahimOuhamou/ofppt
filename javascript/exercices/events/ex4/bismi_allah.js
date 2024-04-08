49e8e6391a62456f4343863809e23b684c3e79b2
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-08 11:02:41 +0000

    alhamdo li Allah

--- javascript/exercices/events/ex4/bismi_allah.js
+++ javascript/exercices/events/ex4/bismi_allah.js
@@ -4,5 +4,7 @@
 document.getElementById('bismi_allah').onselect = function()
 {
     alert('la ilaha illa Allah mohammed rassoul Allah');
+    let selected_text = this.value.substr(this.selectionStart, this.selectionEnd);
+    alert("you have selected '" + selected_text + "'");
 }
 
