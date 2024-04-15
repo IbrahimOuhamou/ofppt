505854b3e5fd0fcc1aaa3a816718e2a3fde42bc3
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-15 11:58:19 +0100

    alhamdo li Allah, astaghfiro Allah wa atobo ilayhi

---
+++ javascript/exercices/events/ex6/bismi_allah.js
@@ -0,0 +1,11 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+bismi_allah_input = document.getElementById("bismi_allah_input");
+bismi_allah_button = document.getElementById("bismi_allah_button");
+
+bismi_allah_button.onclick = function()
+{
+    alert(bismi_allah_input.value)
+}
+
---
+++ javascript/exercices/events/ex7/bismi_allah.js
@@ -0,0 +1,8 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+document.getElementById("bismi_allah_button").onclick = function()
+{
+    document.getElementById("bismi_allah_input2").value = document.getElementById("bismi_allah_input1").value
+}
+
