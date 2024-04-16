344b018c89a0bffff1390c8878cd067fb08da54a
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-16 12:55:51 +0100

    alhamdo li Allah

--- php/php/str.php
+++ php/php/str.php
@@ -4,5 +4,14 @@
 
 echo "in the name of Allah\n";
 
-var_dump(strpos("bismi Allah", "God"));
+$i = strpos("bismi Allah", "Allah");
+echo $i ? $i : "false";
+
+echo strtoupper("\nBismi Allah") . " " . strtolower("Bismi Allah");
+$bismi_allah_str = "\nAllah Akbar\nAllah Akbar\n";
+$bismi_allah_str = str_replace("\n", "\n\n", $bismi_allah_str);
+echo $bismi_allah_str;
+echo "  la ilaha illa Allah mohammed rassoul Allah  \n";
+echo trim("  la ilaha illa Allah mohammed rassoul Allah  \n");
+echo substr("bsd \nbismi Allah fdjkfajkdjkadn", 4, 12);
 
