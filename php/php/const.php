55bd32052d702a638bdf92674517e3e0a20724bc
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-16 13:10:09 +0100

    alhamdo li Allah

---
+++ php/php/const.php
@@ -0,0 +1,10 @@
+<?php
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+echo "in the name of Allah\n";
+
+define('BISMI_ALLAH', 'بسم الله الرحمن الرحيم');
+echo BISMI_ALLAH;
+
+
