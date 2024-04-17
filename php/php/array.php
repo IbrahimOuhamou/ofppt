5e2c9f4f392f3ff1d12b3fc9ea3a4e2086f3c220
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-17 17:20:34 +0100

    la ilaha illa Allah

--- php/php/array.php
+++ php/php/array.php
@@ -25,12 +25,44 @@
         }
         //parcouri un tableau avec foreach
         echo '<br>';
-        sort()
         foreach($tab as $v)
         {
             echo "<li>" . $v . "</li>";
         }
         echo "</ul>";
+
+        //declaration d'un tableau avec []
+        $colors = ["red", "green", "yellow"];
+
+        array_push($colors, "brown");
+
+        echo "<ol>";
+        foreach($colors as $c)
+        {
+            echo "<li>" . $c . "</li>";
+        }
+        echo "</ol>";
+
+        //correction d'exercice
+        $n = rand(2, 20);
+        $arr = [];
+        for($i = 0; $i < $n; $i++)
+        {
+            array_push($arr, rand(-100, 100));
+        }
+
+        echo "n == " . $n . "<br>";
+        foreach($arr as $v)
+        {
+            echo " / " . $v;
+        }
+        echo '<br>';
+        rsort($arr);
+        echo "tableau apres le tri <br />";
+        foreach($arr as $v)
+        {
+            echo " / " . $v;
+        }
     ?>
 </body>
 </html>
