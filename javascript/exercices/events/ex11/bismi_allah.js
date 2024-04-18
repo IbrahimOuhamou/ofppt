42ffe5c6301fabbe86da15709201b9bb877a5b07
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-18 12:36:23 +0100

    alhamdo li Allah

---
+++ javascript/exercices/events/ex11/bismi_allah.js
@@ -0,0 +1,11 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+document.getElementById("bismi_allah_button").onclick = function()
+{
+    if(document.bismi_allah.bismi_allah_select.value)
+    {
+        document.bgColor = document.bismi_allah.bismi_allah_select.value
+    }
+}
+
