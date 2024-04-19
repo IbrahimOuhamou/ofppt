514a2323a9d4feab49397bf89140f97be71a3e28
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-19 12:10:45 +0100

    alhamdo li Allah

--- javascript/exercices/tableau/ex1/bismi_allah.js
+++ javascript/exercices/tableau/ex1/bismi_allah.js
@@ -10,20 +10,34 @@ document.getElementById("bismi_allah_button1").onclick = function()
     let paragraph = '';
 
     let i_min = 0;
+    let i_max = 0;
     for (let i = 0; i < t1.length; i++)
     {
         if(t1[i] < t1[i_min])
         {
             i_min = i;
         }
+        if(t1[i] > t1[i_max])
+        {
+            i_max = i;
+        }
         paragraph += t1[i] + " | ";
     }
-
 
     document.getElementById("bismi_allah_p").innerHTML = "alhamdo li allah valeurs du tableau: " + paragraph;
     document.getElementById("bismi_allah_rsult1-1").innerHTML = "alhamdo li allah min index est: " + i_min;
     document.getElementById("bismi_allah_rsult1-2").innerHTML = "alhamdo li allah min valeur est: " + t1[i_min];
+    document.getElementById("bismi_allah_rsult1-3").innerHTML = "alhamdo li allah max index est: " + i_max;
+    document.getElementById("bismi_allah_rsult1-4").innerHTML = "alhamdo li allah max valeur est: " + t1[i_max];
     document.getElementById("bismi_allah_input1").value = "";
+}
+
+let t2 = [];
+let t2_result = [];
+document.getElementById("bismi_allah_button2-1").onclick = function()
+{
+    t2.push(document.getElementById("bismi_allah_input2").value);
+    document.getElementById("bismi_allah_input2").value = '';
 }
 
 
