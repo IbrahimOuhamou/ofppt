3b7f9129e0e45876d3040772b21c5add6c435fd5
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-23 10:55:22 +0100

    la hawla wa la 9wata illa bi Allh

--- javascript/exercices/str/ex1/bismi_allah.js
+++ javascript/exercices/str/ex1/bismi_allah.js
@@ -1,13 +1,35 @@
 //بسم الله الرحمن الرحيم
 //la ilaha illa Allah mohammed rassoul Allah
 
-bismi_allah_str = prompt("assalamo alaykom\nenter a string please");
+let bismi_allah_str_arr_len = prompt("donner une chaine pour conter leur mots").split(" ").length
+alert("il a " + bismi_allah_str_arr_len + " mots")
+
+function count(str, char)
+{
+    let count = 0;
+    for(const c of str)
+    {
+        if(c === char) count++;
+    }
+    return count;
+}
+
+bismi_allah_str = ""
+while (bismi_allah_str.length<3)
+{
+    bismi_allah_str = prompt("assalamo alaykom\nenter a string please\n(plus de 3 characters)");
+}
 
 bismi_allah_p = document.getElementById("bismi_allah_p")
-
+bismi_allah_p.innerHTML += "<br>";
 
 for(let i=0; i<bismi_allah_str.length; i++)
 {
     bismi_allah_p.innerHTML += bismi_allah_str.substring(0, i+1) + "<br/>";
+}
+
+document.getElementById("bismi_allah_button").onclick = function()
+{
+
 }
 
