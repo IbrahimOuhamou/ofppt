7c0c1255fa4fc07378388ae16623785bd69f2e7e
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-23 09:23:56 +0100

    astaghfiro Allah

--- javascript/javascript/bismi_allah.js
+++ javascript/javascript/bismi_allah.js
@@ -11,5 +11,22 @@ document.f1.bismi_allah_textarea.onclick = function()
     alert(this.value)
 }
 
+let bismi_allah_arr = [];
+for(let i=0; i<10; i++)
+{
+    bismi_allah_arr[i] = [i+1, i+2, i+3]
+}
 
+for(let i=0; i<bismi_allah_arr.length; i++)
+{}
+
+let p2 = document.getElementById("p2");
+for(sub_arr of bismi_allah_arr)
+{
+    for(v2 of sub_arr)
+    {
+        p2.innerHTML += v2 + " |";
+    }
+    p2.innerHTML += "<br>";
+}
 
