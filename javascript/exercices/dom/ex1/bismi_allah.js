ad61608d850222bc758ad3b83d3b7918a0696558
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-06 13:04:39 +0100

    astaghfiro Allah

---
+++ javascript/exercices/dom/ex1/bismi_allah.js
@@ -0,0 +1,13 @@
+//بسم الله الرحمن الرحيم
+//la ilaha illa Allah mohammed rassoul Allah
+
+
+function bismi_allah()
+{
+    arr = document.getElementsByClassName("bismi_allah_p")
+    for (const bismi_allah of arr)
+    {
+        bismi_allah.style.color = "red";
+    }
+}
+
