d011cca7fe1e58043c8d1cbff9bad6f00f8826b4
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-08 12:31:46 +0100

    la ilaha illa Allah

--- javascript/exercices/dom/ex2/bismi_allah.js
+++ javascript/exercices/dom/ex2/bismi_allah.js
@@ -3,8 +3,16 @@
 
 function bismi_allah()
 {
+    let parent = document.getElementById("bismi_allah_div")
     let bismi_allah_p = document.createElement("p")
     bismi_allah_p.innerHTML = "la ilaha illa Allah Mohammed Rassoul Allah"
-    document.getElementById("bismi_allah_div").appendChild(bismi_allah_p)
+    bismi_allah_p.onclick = function() {
+        //this.style.color = "green"
+        //parent.removeChild(this)
+        let bismi_allah_h1 = document.createElement("h1")
+        bismi_allah_h1.innerHTML = "بسم الله الرحمن الرحيم"
+        parent.replaceChild(bismi_allah_h1, this)
+    }
+    parent.appendChild(bismi_allah_p)
 }
 
