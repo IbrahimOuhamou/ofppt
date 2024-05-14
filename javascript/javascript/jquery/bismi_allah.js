2ace69eee4f145750009db0d78d435f2fa962f75
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-14 09:12:03 +0100

    alhamdo li Allah

---
+++ javascript/javascript/jquery/bismi_allah.js
@@ -0,0 +1,7 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+jQuery(document).ready(function() {alert("la ilaha illa Allah Mohammed Rassoul Allah")})
+$("p").addClass("bismi_allah")
+
+
