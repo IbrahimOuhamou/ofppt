8c6077a8a1dacc0235e74f67af47905cad729ee5
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-17 10:15:12 +0100

    alhamdo li Allah added events

--- javascript/javascript/events/bismi_allah.js
+++ javascript/javascript/events/bismi_allah.js
@@ -1,9 +1,19 @@
 //in the name of Allah
 //la ilaha illa Allah mohammed rassoul Allah
 
+let bismi_allah_interval = null;
+
 document.getElementById("bismi_allah").onclick = function() {
     setTimeout(() => {
         alert("بسم الله الرحمن الرحيم")
     }, 2000);
+
+    bismi_allah_interval = setInterval(() => {
+        document.getElementById("bismi_allah_p").innerHTML = "بسم الله الرحمن الرحيم" + "<br/>" + Math.floor(Math.random() * 100)
+    }, 1000);
+}
+
+document.getElementById("bismi_allah2").onclick = function() {
+    clearInterval(bismi_allah_interval)
 }
 
