aa79490716e7a6a033d834ae88569bed9becf312
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-17 12:28:38 +0100

    alhamdo li Allah

---
+++ javascript/javascript/regex/bismi_allah.js
@@ -0,0 +1,9 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+let bismi_allah_regex = /[a-zA-Z\ ]+/
+
+let found = "in the name of Allah".match(bismi_allah_regex)
+
+alert(found)
+
