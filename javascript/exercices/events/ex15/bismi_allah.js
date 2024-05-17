6cae73cc5c557a930f313411f5912da59e9f51ce
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-17 10:25:40 +0100

    la ilaha illa Allah

--- javascript/exercices/events/ex15/bismi_allah.js
+++ javascript/exercices/events/ex15/bismi_allah.js
@@ -1,10 +1,11 @@
 //in the name of Allah
 //la ilaha illa Allah mohammed rassoul Allah
 
-let bismi_allah_interval = null;
-let bismi_allah_temps = 10;
+let bismi_allah_interval = null
+let bismi_allah_temps = 10
 
 document.getElementById("bismi_allah_button").onclick = function() {
+    bismi_allah_temps = 10
     bismi_allah_interval = setInterval(() => {
         bismi_allah_temps -= 1;
         document.getElementById("bismi_allah_h1").innerHTML = "bismi Allah: " + bismi_allah_temps + " seconds restantes"
