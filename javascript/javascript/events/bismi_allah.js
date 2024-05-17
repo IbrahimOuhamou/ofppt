0d2f4472f5d56211c3f08eeaffc459234ba57038
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-17 09:18:49 +0100

    alhamdo li Allah js timeout

---
+++ javascript/javascript/events/bismi_allah.js
@@ -0,0 +1,9 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+document.getElementById("bismi_allah").onclick = function() {
+    setTimeout(() => {
+        alert("بسم الله الرحمن الرحيم")
+    }, 2000);
+}
+
