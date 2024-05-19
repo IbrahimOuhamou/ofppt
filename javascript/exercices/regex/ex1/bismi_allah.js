bd330d7f5c1335d529617b6425899c449d84e608
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-19 20:10:56 +0100

    alhamdo li Allah

--- javascript/exercices/regex/ex1/bismi_allah.js
+++ javascript/exercices/regex/ex1/bismi_allah.js
@@ -1,3 +1,11 @@
 //in the name of Allah
 //la ilaha illa Allah mohammed rassoul Allah
 
+document.getElementById("bismi_allah_env_tech_s").onchange = function() {
+    document.getElementById("bismi_allah_env_tech_ta").value = this.value
+}
+
+document.getElementById("bismi_allah_button").onclick = function() {
+    alert("بسم الله الرحمن الرحيم")
+}
+
