77cf3b21bccd056a7fa921a610d61f2675a60949
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-23 18:37:50 +0100

    alhamdo li Allah did exercise

---
+++ php/php/supp_stag.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--بسم الله الرحمن الرحيم-->
+
+<?php
+    try {
+        $connexion = new PDO('mysql:host=192.168.122.126;dbname=myDB;', 'bismi_allah', 'bismi_allah');
+
+        $query = $connexion->prepare('DELETE FROM stagiaires WHERE cin=?');
+        $query->bindValue(1, $_REQUEST['cin']);
+        $query->execute();
+
+    } catch(Exception $e) {
+        echo $e->getMessage();
+    }
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>bismi Allah</title>
+    </head>
+    <body>
+        <p>بسم الله الرحمن الرحيم</p>
+        <p>alhamdo li Allah deleted user with cin: <?php echo $_REQUEST['cin'] ?></p>
+        <p><a href="/php/listStagiaires.php">list des stagiaires</a></p>
+    </body>
+</html>
