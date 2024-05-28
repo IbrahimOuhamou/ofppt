cba140ad00954ad19b1bca2ddd5e278ef40d7cf1
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-28 22:07:05 +0100

    alhamdo li Allah'

---
+++ javascript/regios/27-5-2024/bismi_allah.js
@@ -0,0 +1,14 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+let bismi_allah_donnees = {
+    "consoles": [
+        { 'ref': 'Nintendo Switch Oled', 'prix': 4500, 'Manette': 1, 'Image': 'images/img1.jpg'},
+        { 'ref': '', 'prix': 4500, 'Manette': 1, 'Image': 'images/img2.jpg'},
+    ]
+}
+
+document.getElementById("bismi_allah_button").onclick = function() {
+    
+}
+
