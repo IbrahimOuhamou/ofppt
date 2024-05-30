52da0094b31e2c44a59d6303bad43af21fb98d73
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-30 13:12:59 +0100

    astaghfiro Allah

---
+++ javascript/regios/30-5-2024/bismi_allah.js
@@ -0,0 +1,45 @@
+//بسم الله الرحمن الرحيم
+//la ilaha illa Allah Mohammed Rassoul Allah
+
+document.getElementById("button_enregister").onclick = function() {
+    if(!validerForm()) {
+        alert("valider lo formulaire!")
+        return
+    }
+
+    let row = document.createElement("tr")
+
+    let nom = document.createElement("td")
+    nom.innerHTML = document.getElementById("input_nom").value
+    row.appendChild(nom)
+
+    let prenom = document.createElement("td")
+    prenom.innerHTML = document.getElementById("input_prenom").value
+    row.appendChild(prenom)
+
+    let age = document.createElement("td")
+    age.innerHTML = document.getElementById("input_age").value
+    row.appendChild(age)
+
+    let sexe = document.createElement("td")
+    sexe.innerHTML = document.getElementById("input_sexe_homme").checked ? 'homme' : 'femme'
+    row.appendChild(sexe)
+
+    let pays = document.createElement("td")
+    pays.innerHTML = document.getElementById("select_pays").value
+    row.appendChild(pays)
+
+    document.getElementById("enregitrers_table").appendChild(row)
+}
+
+function validerForm() {
+    if(document.getElementById("input_nom").value == "") return false
+    if(document.getElementById("input_prenom").value == "") return false
+    if(document.getElementById("input_age").value == "") return false
+    if(document.getElementById("input_sexe_femme").checked == document.getElementById("input_sexe_homme").checked) return false
+
+    if(document.getElementById("input_checkbox_marathon").checked || document.getElementById("input_checkbox_natation").checked || document.getElementById("input_checkbox_sprint").checked || document.getElementById("input_checkbox_saut").checked)
+
+    return true
+}
+
