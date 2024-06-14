505d5191319ffbf7ec829a83ec40464160b0eefa
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-06-14 11:20:41 +0100

    la ilaha illa Allah

---
+++ mysql/entreprise.sql
@@ -0,0 +1,42 @@
+-- بسم الله الرحمن الرحيم
+-- la ilaha illa Allah Mohammed Rassoul Allah
+
+CREATE DATABASE IF NOT EXISTS MaBase;
+
+use MaBase;
+
+
+DROP TABLE IF EXISTS Employes;
+CREATE TABLE Employes (
+	id INT,
+	nom VARCHAR(100),
+	prenom VARCHAR(100),
+	age INT,
+	salaire DECIMAL(10, 2)
+);
+
+DROP TABLE IF EXISTS Division;
+
+CREATE TABLE Division(
+	id INT PRIMARY KEY,
+	nom VARCHAR(100),
+	NbrEmp INT,
+	empl_id INT,
+	CONSTRAINT fk_division FOREIGN KEY (empl_id) REFERENCES Employes(id)
+);
+
+INSERT INTO Employes (id, nom, prenom, age, salaire) VALUES 
+	(1, 'Dupont', 'Jean', 30, 2500.00),
+	(2, 'Dup', 'Ali', 25, 5000.00);
+
+ALTER TABLE Division ADD COLUMN adresse VARCHAR(255);
+
+-- ALTER TABLE Division ADD CONSTRAINT fk_division FOREIGN KEY (empl_id) REFERENCES Employes(id);
+-- ALTER TABLE Division ADD CONSTRAINT fk_division FOREIGN KEY (empl_id) REFERENCES Employes(id);
+
+
+ALTER TABLE Employes ADD CONSTRAINT check_age CHECK (age >= 18);
+
+SELECT * FROM Division;
+SELECT * FROM Employes;
+
