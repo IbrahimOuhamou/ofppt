cae496589ae05c894b77cc300594733e59a0e48d
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-06-21 11:28:10 +0100

    alhamdo li Allah

---
+++ mysql/exercices/ex1.sql
@@ -0,0 +1,18 @@
+-- بسم الله الرحمن الرحيم
+-- la ilaha illa Allah Mohammed Rassoul Allah
+
+
+-- 1
+
+UPDATE ETUDIANT SET niveauEtu = 'Bac+5' WHERE niveauEtu = 'Master';
+UPDATE ETUDIANT SET niveauEtu = 'Bac+4' WHERE niveauEtu = 'Bachelor';
+UPDATE ETUDIANT SET niveauEtu = 'Phd' WHERE niveauEtu = 'Doctorat';
+
+-- 2
+
+UPDATE ETUDIANT SET titre = 'develeloppement de Jave' WHERE id=11;
+
+-- 3
+
+ALTER TABLE ;
+
