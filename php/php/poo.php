6a2ba991c1f14d07007e2a0911d81e6ba3003714
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-06-25 19:59:24 +0100

    alhamdo li Allah

--- php/php/poo.php
+++ php/php/poo.php
@@ -6,9 +6,37 @@ class Produit {
     //Declaration
     public $code;
     public $designation;
-    public $prix;
+    private $prix;
+    public const TVA = 1.5;
 
-    function set_prix($prix) {
+    private static $nbrProduit = 0;
+
+    function __get($param) {
+        echo 'alhamdo li Allah tried to __get ' . $param . "\n";
+    }
+
+    function __construct($code = 1, $designation = 1, $prix = 1) {
+
+        Produit::$nbrProduit++;
+
+        $this->code = $code;
+        $this->designation = $designation;
+        $this->setPrix($prix);
+    }
+
+    function get_nbr() {
+        return Produit::$nbrProduit;
+    }
+
+    function json_encode() {
+        return json_encode(get_object_vars($this));
+    }
+
+    public function __toString() {
+        return 'Code:' . $this->code . ' designation:' . $this->designation . ' prix' . $this->getPrix();
+    }
+
+    function setPrix($prix) {
         if($prix > 0) {
             $this->prix = $prix;
         } else {
@@ -16,18 +44,23 @@ class Produit {
         }
     }
 
-    function get_prix() {
-        return $this->prix * 1.5;
+    function getPrix() {
+        return $this->prix * Produit::TVA;
     }
-
 }
 
-$produit1 = new Produit();
+$produit1 = new Produit(10, 'PC', -5);
+$produit2 = new Produit(30, 'STYLO', 1000);
 
-$produit1->code = -5;
-$produit1->designation = 'PC';
-$produit1->prix = 5000;
+echo $produit1;
+echo "\n" . $produit1->json_encode();
+echo "\n" . $produit2;
+echo "\n" . $produit2->json_encode();
 
-var_dump($produit1);
+new Produit(30, 'STYLO', 1000);
+new Produit(30, 'STYLO', 1000);
+new Produit(30, 'STYLO', 1000);
+echo "\n" . $produit1->get_nbr();
 
 ?>
+
