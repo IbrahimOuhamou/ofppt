e30fb50bc078035e5c76572a164e487a19b4e535
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-04 10:04:52 +0000

    astaghfiro Allah

--- cloud/mongodb/index.js
+++ cloud/mongodb/index.js
@@ -3,26 +3,46 @@
 
 const express = require('express');
 const app = express();
-const port = 3800;
+const port = 3000;
 
 const mongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://127.0.0.1:27017';
-const dbName = 'bismi_allah_db';
-const collectionName1 = 'bismi_allah';
+const url = 'mongodb://localhost:27017/';
+const dbName = 'alhamdo-li-allah';
+const collectionName1 = 'equipes';
 
 //const collectionName2 = 'joueur';
 //const client = new mongoClient.MongoClient(url,(err,client) => { Mydb=client.db('collectionName1');});
+//
+// let mydb = mongoClient.connect(url, (err,client) => { 
+//     if(err) { console.error(err); }
+//     console.log("بسم الله الرحمن الرحيم");
+//     mydb=client.db(collectionName1);
+//     console.log("connection established");
+// });
 
-let mydb = mongoClient.connect(url,(err,client) => { 
-    if(err) { console.error(err); }
-    console.log("بسم الله الرحمن الرحيم");
-    mydb=client.db(collectionName1);
-    console.log("connection established");
-});
+mongoClient.connect(
+    url,
+    (err,client) => { 
+        console.log('alhamdo li ALlah connected to db');
+    })
+    .then(
+        client => mydb = client.db(dbName),
+            app.get("/equipes", async (req, res) => {
+                const items = await mydb.collection(collectionName1).find({}).toArray();
+                res.json(items);
+            }),
+
+            app.get("/equipes:id", async (req, res) => {
+                const id = Number(req.params.id);
+                const items = await mydb.collection(collectionName1).find({id:id}).toArray();
+                res.json(items);
+            })
+        )
+    )
 
 app.get('/', (req, res) => {
     console.log("بسم الله الرحمن الرحيم");
-    res.status(200);
+    res.status(200).send('بسم الله الرحمن الرحيم');
 });
 
 app.listen(port, () => { console.log("بسم الله الرحمن الرحيم\nport:", port) });
